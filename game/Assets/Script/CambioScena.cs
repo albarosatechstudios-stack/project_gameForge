339b2698fa82c774091f93da1b175297eb7f1727
Giuseppe Zoltan Bruniani <81922012+brngpp@users.noreply.github.com>
2025-12-04 14:40:02 +0100

    add cambio scena

---
+++ game/Assets/Script/CambioScena.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // <--- FONDAMENTALE!
+
+public class CambioScena : MonoBehaviour
+{
+    [Header("Nome della scena dove andare")]
+    public string nomeScenaDaCaricare; // Scrivi qui il nome esatto (es. "Livello2")
+
+    // Metodo 1: Se entri in un Trigger (es. una porta)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Cambio scena in corso...");
+            SceneManager.LoadScene(nomeScenaDaCaricare);
+        }
+    }
+
+    // Metodo 2: Se vuoi chiamarlo da un Bottone della UI
+    public void CaricaScena()
+    {
+        SceneManager.LoadScene(nomeScenaDaCaricare);
+    }
+}
