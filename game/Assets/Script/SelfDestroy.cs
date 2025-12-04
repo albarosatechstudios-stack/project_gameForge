e2018d5f91136d69c219d23d772ac181f37057e3
Domenico Violi <dome.violi90@gmail.com>
2025-12-04 17:55:46 +0100

    fix  problematiche fumogeno + collider

--- game/Assets/Script/SelfDestroy.cs
+++ game/Assets/Script/SelfDestroy.cs
@@ -1,14 +1,35 @@
 using UnityEngine;
+using System.Collections;
 
 public class SelfDestroy : MonoBehaviour
 {
     [Header("Impostazioni Tempo")]
     [Tooltip("Dopo quanti secondi l'oggetto deve distruggersi?")]
-    public float lifeTime = 60f; // Default: 1 minuto
+    public float lifetime = 30f; // Default: 1 minuto
+    private SpawnFumogeno player;         // riferimento al player
+
 
     void Start()
     {
+        player = FindFirstObjectByType<SpawnFumogeno>();
+
         // Pianifica la distruzione di questo GameObject dopo 'lifeTime' secondi
-        Destroy(gameObject, lifeTime);
+       StartCoroutine(DestroyAfterTime());
+    }
+
+    IEnumerator DestroyAfterTime()
+    {
+        // aspetta la lifetime
+        yield return new WaitForSeconds(lifetime);
+
+        // Ripristina l'item al player
+        if (player != null)
+        {
+            player.RestoreItem();
+            Debug.Log("fumogeno ripristinato al player!");
+        }
+
+        // Distruggi l'oggetto
+        Destroy(gameObject);
     }
 }
